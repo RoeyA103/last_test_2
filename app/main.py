7bf306896f7bf55990e1f5bdad6acbcee1c9edfa
roey_averbuch <roey6922@gmail.com>
2026-03-17 18:18:21 +0200

    fixing host in main

--- app/main.py
+++ app/main.py
@@ -10,4 +10,4 @@ def root():
     return {"message":"service healthy"}
 
 if __name__=="__main__":
-    uvicorn.run("main:app",host="localhost",port=8080,reload=True)
+    uvicorn.run("main:app",host="0.0.0.0",port=8080,reload=True)
